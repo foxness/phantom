30ceeef0b55c907cc175506b29bea72f58bc1e3d
River <foxnezz@gmail.com>
2020-08-29 12:54:16 +0200

    Initial Commit

---
+++ Phantom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Phantom
+//
+//  Created by user179800 on 8/29/20.
+//  Copyright © 2020 Rivershy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
