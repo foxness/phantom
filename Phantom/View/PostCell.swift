68f6d270ffce94d6cd8a925642be8a698a3cff17
River <foxnezz@gmail.com>
2021-05-13 15:44:32 +0300

    [REFACTOR] refactored post cell

--- Phantom/View/PostCell.swift
+++ Phantom/View/PostCell.swift
@@ -12,10 +12,10 @@ import Kingfisher
 class PostCell: UITableViewCell {
     static let IDENTIFIER = "PostCell"
     
-    @IBOutlet weak var ptitle: UILabel!
-    @IBOutlet weak var ptext: UILabel!
-    @IBOutlet weak var pimage: UIImageView!
-    @IBOutlet weak var pdate: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var thumbnailView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -28,16 +28,37 @@ class PostCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
-    func set(post: Post) {
+    func setPost(_ post: Post) {
+        setMainViews(for: post)
+        setThumbnail(for: post)
+        setBackground(for: post)
+        
+//        let f = UIScreen.main.scale
+//        let w = pimage.bounds.width * f
+//        let h = pimage.bounds.height * f
+//
+//        Log.p("viewW: \(w), viewH: \(h)")
+//
+//        let ss = pimage.image!.scale
+//        let ww = pimage.image!.size.width * ss
+//        let hh = pimage.image!.size.height * ss
+//
+//        Log.p("imgW: \(ww), imgH: \(hh)")
+    }
+    
+    private func setMainViews(for post: Post) {
         let title = post.title
         let subtitle = post.type == .text ? post.text : post.url
         let date = PostCell.dateToString(post.date)
         
-        ptitle.text = title
-        ptext.text = subtitle
-        pdate.text = date
+        titleLabel.text = title
+        subtitleLabel.text = subtitle
+        dateLabel.text = date
+    }
+    
+    private func setThumbnail(for post: Post) {
+        var shouldSetPlaceholder = true
         
-        //pimage.image = UIImage(named: "thumbnail_text_post")!
         if post.type == .link, let url = post.url {
             var imageUrl: String? = nil
             if let wallhavenThumbnailUrl = Wallhaven.getThumbnailUrl(wallhavenUrl: url) {
@@ -47,30 +68,23 @@ class PostCell: UITableViewCell {
             }
             
             if let imageUrl = imageUrl, let goodImageUrl = URL(string: imageUrl) {
-                pimage.kf.setImage(with: goodImageUrl)
+                thumbnailView.kf.setImage(with: goodImageUrl)
+                shouldSetPlaceholder = false
             }
-        } else {
-            pimage.image = UIImage(named: "thumbnail_text_post")!
         }
         
+        if shouldSetPlaceholder {
+            thumbnailView.image = UIImage(named: "thumbnail_text_post")!
+        }
+    }
+    
+    private func setBackground(for post: Post) {
         let overdue = post.date < Date()
         let bg: UIColor = overdue ? .secondarySystemBackground : .systemBackground
         contentView.backgroundColor = bg
-        
-        /*let f = UIScreen.main.scale
-        let w = pimage.bounds.width * f
-        let h = pimage.bounds.height * f
-        
-        Log.p("viewW: \(w), viewH: \(h)")
-        
-        let ss = pimage.image!.scale
-        let ww = pimage.image!.size.width * ss
-        let hh = pimage.image!.size.height * ss
-        
-        Log.p("imgW: \(ww), imgH: \(hh)")*/
     }
     
-    static func dateToString(_ date: Date) -> String { // in X hours
+    private static func dateToString(_ date: Date) -> String { // "in X hours"
         let formatter = RelativeDateTimeFormatter()
         formatter.unitsStyle = .full
         return formatter.localizedString(for: date, relativeTo: Date())
