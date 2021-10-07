5200f0f6a9cd5a4f4fbe4a9bdd0b2ebcf17e554f
River <foxnezz@gmail.com>
2021-10-08 01:56:07 +0300

    [FIX] fixed invisible welcome screen sign in button in iOS 14

--- Phantom/View/ViewController/IntroductionViewController.swift
+++ Phantom/View/ViewController/IntroductionViewController.swift
@@ -9,9 +9,6 @@
 import UIKit
 
 // todo: add reddit icon into button?
-// todo: add real app icon
-// todo: better fonts
-// todo: better spacing
 // todo: rename to WelcomeViewController? [ez]
 
 class IntroductionViewController: UIViewController {
@@ -19,10 +16,19 @@ class IntroductionViewController: UIViewController {
         case showRedditSignIn = "introductionShowRedditSignIn"
     }
     
+    private static let BUTTON_CORNER_RADIUS_IOS14: CGFloat = 9
+    private static let HIGHLIGHT_TINT_PERCENT_IOS14: CGFloat = 20
+    
     @IBOutlet weak var signInButton: UIButton!
     @IBOutlet weak var appIconView: UIImageView!
     
     private let hideNavBar = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        styleButtonForIos14()
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -38,6 +44,30 @@ class IntroductionViewController: UIViewController {
         if hideNavBar {
             navigationController?.navigationBar.isHidden = start
         }
+    }
+    
+    func styleButtonForIos14() {
+        if #available(iOS 15.0, *) {
+            // intentionally nothing, already styled for iOS 15 in storyboard with iOS 15 filled button style
+        } else { // iOS 14
+            let tintColor = UIColor(named: "AccentColor")!
+            
+            signInButton.backgroundColor = tintColor
+            signInButton.layer.cornerRadius = IntroductionViewController.BUTTON_CORNER_RADIUS_IOS14
+            signInButton.layer.masksToBounds = true
+            
+            let lighterTint = tintColor.lighter(by: IntroductionViewController.HIGHLIGHT_TINT_PERCENT_IOS14)
+            let darkerTint = tintColor.darker(by: IntroductionViewController.HIGHLIGHT_TINT_PERCENT_IOS14)
+            let highlightColor = traitCollection.userInterfaceStyle == .dark ? lighterTint : darkerTint
+
+            signInButton.setBackgroundColor(highlightColor, for: .highlighted)
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        styleButtonForIos14()
     }
 }
 
